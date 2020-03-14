7d424790626a63309105b53140dff405f82692c6
nancywen25 <nwen2011@gmail.com>
2020-03-13 23:10:27 -0400

    add function to deal with unknown words

--- run_hmm.py
+++ run_hmm.py
@@ -2,6 +2,29 @@ import itertools
 import numpy as np
 from train_hmm import read_data, get_transition_probs, get_emission_probs
 
+def lookup_emission_prob(word, tag_idx, word_idx, state=None):
+    """
+
+    Args:
+        word: word to lookup
+        tag_idx: dict with mapping from tag to index
+        word_idx: dict with mapping from word in index
+        state: index of state, default None to get probs for all states
+    Returns:
+        float: emission probability of one state
+        numpy array of floats: emission probability of all states
+    """
+    if word in word_idx: # known word
+        if state:
+            return B[state, word_idx[word]]
+        else:
+            return B[:, word_idx[word]]
+    else:   # use a uniform distribution for unknown words
+        if state:
+            return 1/len(tag_idx)
+        else:
+            return np.ones(len(tag_idx))/len(tag_idx)
+
 def run_viterbi(obs_seq, tag_idx, word_idx, start_prob, A, B):
     """
 
@@ -20,8 +43,6 @@ def run_viterbi(obs_seq, tag_idx, word_idx, start_prob, A, B):
         best_path: list representing tags in most probable path
         best_path_prob: probability of most probable path
     """
-    obs_seq = [word_idx[word] for word in obs_seq] # TODO still have to deal with unknown words
-
     # path probability matrix with N rows and T columns
     viterbi = np.zeros((len(tag_idx), len(obs_seq)))
 
@@ -30,21 +51,20 @@ def run_viterbi(obs_seq, tag_idx, word_idx, start_prob, A, B):
 
 
     # initialization step
-    viterbi[:, 0] = start_prob * B[:, obs_seq[0]]
+    viterbi[:, 0] = start_prob * lookup_emission_prob(obs_seq[0], tag_idx, word_idx)
 
     # recursion step
     for t in range(1, len(obs_seq)): # loop through each token in sentence
         for s in range(len(tag_idx)): # loop through all states
-            viterbi[s, t] = np.max(viterbi[:, t-1] * A[:, s] * B[s, obs_seq[t]])
-            backpointer[s, t] = np.argmax(viterbi[:, t-1] * A[:, s] * B[s, obs_seq[t]])
-
+            viterbi[s, t] = np.max(viterbi[:, t - 1] * A[:, s] * lookup_emission_prob(obs_seq[t], tag_idx, word_idx, s))
+            backpointer[s, t] = np.argmax(viterbi[:, t - 1] * A[:, s] * lookup_emission_prob(obs_seq[t], tag_idx, word_idx, s))
     # termination step
     best_path_prob = np.max(viterbi[:, -1])
     best_path_pointer = np.argmax(viterbi[:, -1])
 
     best_path = np.empty(len(obs_seq), dtype=int)
     best_path[-1] = best_path_pointer
-    for i in reversed(range(1, 25)):
+    for i in reversed(range(1, len(obs_seq))):
         best_path[i-1] = backpointer[best_path[i], i]
 
     return list(best_path), best_path_prob
@@ -78,8 +98,6 @@ sentences = [list(v) for k, v in itertools.groupby(lines, key=bool) if k]
 
 obs_seq = sentences[0]
 best_path, bestpath_prob = run_viterbi(obs_seq, tag_idx, word_idx, start_prob, A, B)
-
 print_prediction(obs_seq, tag_idx, best_path)
-
 
 
